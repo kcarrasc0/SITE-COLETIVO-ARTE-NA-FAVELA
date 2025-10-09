32185511bc11eb8fdcf0fe28444436bf1e6a2ffc
Erick Carrasco Oliveira <104008021+kcarrasc0@users.noreply.github.com>
2025-10-09 10:30:18 +0000

    fefef

--- src/components/Atividades/Atividades.jsx
+++ src/components/Atividades/Atividades.jsx
@@ -6,22 +6,22 @@ const atividades = [
   {
     titulo: "Comunicação Ambiental",
     descricao: "Capacitação de jovens para se tornarem comunicadores e agentes de mudança, com foco em Clima, Saúde e Tecnologia.",
-    imagem: "../../assets/images/atividade/atividade1.jpg"
+    imagem: "../../src/assets/images/atividade/atividade1.jpg"
   },
   {
     titulo: "Batalha Cultural",
     descricao: "A Batalha Cultural dá voz e identidade à juventude, tornando a rima um instrumento de debate e conscientização no Ibura.",
-    imagem: "../../assets/images/atividade/atividade2.jpg"
+    imagem: "../../src/assets/images/atividade/atividade2.jpg"
   },
   {
     titulo: "Break nas Escolas",
     descricao: "Levamos a arte do break e os valores do hip-hop para escolas da periferia como forma de empoderamento e superação.",
-    imagem: "../../assets/images/atividade/atividade3.jpg"
+    imagem: "../../src/assets/images/atividade/atividade3.jpg"
   },
   {
     titulo: "Cine Ibura",
     descricao: "Nosso cineclube comunitário que promove exibições e debates sobre questões sociais, culturais e ambientais.",
-    imagem: "../../assets/images/atividade/atividade4.jpg"
+    imagem: "../../src/assets/images/atividade/atividade4.jpg"
   },
   
 ];
@@ -30,17 +30,17 @@ const atividades2 = [
   {
     titulo: "Resenha IB",
     descricao: "Um projeto voltado ao fortalecimento de MCs e artistas da cena hip-hop local, oferecendo estrutura para gravação de músicas.",
-    imagem: "../../assets/images/atividade/atividade5.png"
+    imagem: "../../src/assets/images/atividade/atividade5.png"
   },
   {
     titulo: "Periferia Tech",
     descricao: "Oferecemos cursos de tecnologia de forma remota, ensinando boas práticas do mercado de trabalho para o dia a dia.",
-    imagem: "../../assets/images/atividade/atividade6.jpg"
+    imagem: "../../src/assets/images/atividade/atividade6.jpg"
   },
   {
     titulo: "EcoVision",
     descricao: "Web App de monitoramento ambiental criado nas aulas de Tecnologia para acompanhar dados de desmatamento.",
-    imagem: "../../assets/images/atividade/atividade7.png"
+    imagem: "../../src/assets/images/atividade/atividade7.png"
   }
 ]
 
